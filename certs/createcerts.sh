6f1bb8dade3e60dfec37b5412268af7202dbcf7b
DMXMax <29312988+DMXMax@users.noreply.github.com>
2024-06-09 09:23:47 -0700

    updte certificate generation

--- certs/createcerts.sh
+++ certs/createcerts.sh
@@ -5,11 +5,11 @@ set -e
 echo "Generating CA key and certificate:"
 openssl req -x509 -sha256 -noenc -days 7 -newkey rsa:2048 \
   -keyout ca.key -out ca.pem \
-  -subj "/O=quic-go Certificate Authority/"
+  -subj "/O=Nodda Certificate Authority/"
 
 echo "Generating CSR"
 openssl req -out cert.csr -new -newkey rsa:2048 -noenc -keyout private.key \
-  -subj "/O=quic-go/"
+  -subj "/O=quic-go-samples/"
 
 echo "Sign certificate:"
 openssl x509 -req -sha256 -days 7 -in cert.csr  -out cert.pem \
